c2ac658082a116e63910b491566d45e3bcdc8d3a
Josancev <josemanuel777@hotmail.com>
2019-08-05 23:09:27 +0000

    final2

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -7,43 +7,38 @@ export class Traffic extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			SelectedLight: null
+			lightC: null
 		};
 	}
 
 	render() {
 		//console.log(this.state);
-		let redExtraClass = "";
-		if (this.state.SelectedLight == "red") redExtraClass = "selected";
-		let yellowExtraClass = "";
-		if (this.state.SelectedLight == "yellow") yellowExtraClass = "selected";
-		let greenExtraClass = "";
-		if (this.state.SelectedLight == "green") greenExtraClass = "selected";
+		let redClass = "";
+		if (this.state.lightC == "red") redClass = "selected";
+		let yellowClass = "";
+		if (this.state.lightC == "yellow") yellowClass = "selected";
+		let greenClass = "";
+		if (this.state.lightC == "green") greenClass = "selected";
 		return (
 			<div>
 				<div id="trafficTop" />
 				<div id="container">
 					<div
-						className={"red light " + redExtraClass}
-						onClick={() => this.setState({ SelectedLight: "red" })}
+						className={"red light " + redClass}
+						onClick={() => this.setState({ lightC: "red" })}
 					/>
 					<div
-						className={"yellow light " + yellowExtraClass}
-						onClick={() =>
-							this.setState({ SelectedLight: "yellow" })
-						}
+						className={"yellow light " + yellowClass}
+						onClick={() => this.setState({ lightC: "yellow" })}
 					/>
 					<div
-						className={"green light " + greenExtraClass}
-						onClick={() =>
-							this.setState({ SelectedLight: "green" })
-						}
+						className={"green light " + greenClass}
+						onClick={() => this.setState({ lightC: "green" })}
 					/>
 				</div>
 				<div className="pt-5">
 					<h3>
-						<p>On click change the</p>
-						<p>traffic light colors!</p>
+						<p>Traffic Lights</p>
 					</h3>
 				</div>
 			</div>
