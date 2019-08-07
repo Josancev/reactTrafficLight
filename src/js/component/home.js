07cf759f7042ad8e2662708b6cb25d7afa93dde0
Josancev <josemanuel777@hotmail.com>
2019-08-07 04:34:38 +0000

    final

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -4,12 +4,44 @@ import React from "react";
 
 //create your first component
 export class Traffic extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			lightC: null
+		};
+	}
+
 	render() {
+		//console.log(this.state);
+		let redClass = "";
+		if (this.state.lightC == "red") redClass = "on";
+		let yellowClass = "";
+		if (this.state.lightC == "yellow") yellowClass = "on";
+		let greenClass = "";
+		if (this.state.lightC == "green") greenClass = "on";
 		return (
 			<div>
-				<h1> Landing Page</h1>{" "}
+				<div id="trafficTop" />
+				<div id="container">
+					<div
+						className={"red light " + redClass}
+						onClick={() => this.setState({ lightC: "red" })}
+					/>
+					<div
+						className={"yellow light " + yellowClass}
+						onClick={() => this.setState({ lightC: "yellow" })}
+					/>
+					<div
+						className={"green light " + greenClass}
+						onClick={() => this.setState({ lightC: "green" })}
+					/>
+				</div>
+				<div>
+					<h3>
+						<p>Traffic Lights</p>
+					</h3>
+				</div>
 			</div>
-			//console.log(this.state);
 		);
 	}
 }
